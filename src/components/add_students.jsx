438da5f348103d0a49affea6e790a1de4105b117
Ghulamkabira <ghulamkabira622@gmail.com>
2024-10-18 00:47:33 -0700

    remove comits

--- src/components/add_students.jsx
+++ src/components/add_students.jsx
@@ -21,7 +21,7 @@ const AddStudent = () => {
       });
   }, []);
 
-  // Yup validation schema
+  
   const validationSchema = Yup.object({
     name: Yup.string().required('Name is required'),
     email: Yup.string().email('Invalid email format').required('Email is required'),
